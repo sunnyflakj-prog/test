76b7a236ee4cd4806a42de0a80753303350ec332
sunnyflakj-prog <sunnyflakj@gmail.com>
2025-12-19 07:14:23 +0000

    3 Task

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,18 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [lightOn, setLightOn] = useState(false);
-
+    const [text, setText] = useState('Привет!');
+    const toggleText = () => {
+    setText(text === 'Привет!' ? 'Пока!' : 'Привет!');
+    };
   return (
         <>
-    <h2>Счетчик</h2>   
-    <div className="counter-section">
-      <p className="counter-value">{count}</p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-    </div>
-     
-      <div className="app-container">
-        <h2>Переключатель</h2>
-        <p>{lightOn ? 'Свет включен' : 'Свет выключен'}</p>
-        <button onClick={() => setLightOn(!lightOn)}>
-          {lightOn ? 'Выкл' : 'Вкл'}
-        </button>
-      </div>
+    <div>
+      <h2>Изменение текста</h2>
+      <p>{text}</p>
+      <button onClick={toggleText}>
+        Изменить текст
+      </button>    </div>
        </>
   );
 }
