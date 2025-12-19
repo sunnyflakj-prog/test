622515b80621fc6d756e50e5262945a23ef7f0bd
sunnyflakj-prog <sunnyflakj@gmail.com>
2025-12-19 05:59:21 +0000

    count

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount1((count1) => count1 + 1)}>
+          count is {count1}
         </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount2((count2) => count2 + 1)}>
+          count is {count2}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
