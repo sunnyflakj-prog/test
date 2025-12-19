9fdf11c002acd5490c8a9068aa10c5aa02622c79
sunnyflakj-prog <sunnyflakj@gmail.com>
2025-12-19 06:33:38 +0000

    1,2 Task

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count1, setCount1] = useState(0)
-  const [count2, setCount2] = useState(0)
+  const [count, setCount] = useState(0);
+  const [lightOn, setLightOn] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount1((count1) => count1 + 1)}>
-          count is {count1}
+        <>
+    <div className="counter-section">
+      <p className="counter-value">{count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+    </div>
+     
+      <div className="app-container">
+        <h2>Переключатель</h2>
+        <p>{lightOn ? 'Свет включен' : 'Свет выключен'}</p>
+        <button onClick={() => setLightOn(!lightOn)}>
+          {lightOn ? 'Выкл' : 'Вкл'}
         </button>
-        <button onClick={() => setCount2((count2) => count2 + 1)}>
-          count is {count2}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+       </>
+  );
 }
 
-export default App
+export default App;
